59ef66fd21edb4ffb7fe9b245c169ee2c0dfebf2
dustinddoan <dustin.ddoan@gmail.com>
2023-05-31 23:27:37 -0700

    add spring security with OktaOAuth2

---
+++ CloudGateway/src/main/java/com/dailycode/cloudgateway/security/OktaOAuth2WebSecurity.java
@@ -0,0 +1,29 @@
+package com.dailycode.cloudgateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OktaOAuth2WebSecurity {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return http.build();
+    }
+}
+
+// 9D8kWs62 dustin.dev.acc
+// Dq7W9c99 casper.com
